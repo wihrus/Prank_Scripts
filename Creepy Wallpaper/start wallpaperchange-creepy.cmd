034c276ead3e4669edb99a41512e11345bae612c
wihrus <144267415+wihrus@users.noreply.github.com>
2024-06-21 12:55:17 +0200

    Delete Creepy Wallpaper/start wallpaperchange-creepy.cmd

--- Creepy Wallpaper/start wallpaperchange-creepy.cmd
+++
@@ -1,3 +0,0 @@
-mkdir c:\temp\fun
-copy change-Wallpaper-Creepy.ps1 "C:\temp\fun\change-Wallpaper-Creepy.ps1"
-powershell -w h -NoP -NonI -Exec Bypass $pl = "C:\temp\fun\change-Wallpaper-Creepy.ps1" ?dl=1; invoke-expression $pl
