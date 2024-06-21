534a904a1615307c8309ec25b7f057cbfc2c4a3c
wihrus <144267415+wihrus@users.noreply.github.com>
2024-06-21 12:32:22 +0200

    Add files via upload

---
+++ Creepy Wallpaper/start wallpaperchange-creepy.cmd
@@ -0,0 +1,3 @@
+mkdir c:\temp\fun
+copy change-Wallpaper-Creepy.ps1 "C:\temp\fun\change-Wallpaper-Creepy.ps1"
+powershell -w h -NoP -NonI -Exec Bypass $pl = "C:\temp\fun\change-Wallpaper-Creepy.ps1" ?dl=1; invoke-expression $pl
